5194f44360497259a7a1dd522d627069c96f1256
Mahi-Shrma <mahisharmamgs6@gmail.com>
2025-02-04 12:47:56 +0530

    new changes

--- src/pages/ComparePage.js
+++ src/pages/ComparePage.js
@@ -138,17 +138,22 @@ async function handleDaysChange(e) {
       const data = await getCoinData(event.target.value);
       coinObject(setCrypto2Data, data);
       const prices1 = await getCoinPrices(crypto1, days, priceType);
-      const prices2 = await getCoinPrices(crypto2, days, priceType);
-      if (prices1.length > 0 && prices2.length > 0) {
-        setIsLoading(false);
-      }
+      const prices2 = await getCoinPrices(event.target.value, days, priceType);
+      settingChartData(setChartData, prices1, prices2);
+      // if (prices1.length > 0 && prices2.length > 0) {
+      //   setIsLoading(false);
+      // }
     } else {
       setCrypto1(event.target.value);
       const data = await getCoinData(event.target.value);
       coinObject(setCrypto1Data, data);
-      setIsLoading(false);
+      const prices1 = await getCoinPrices(event.target.value, days, priceType);
+      const prices2 = await getCoinPrices(crypto2, days, priceType);
+      settingChartData(setChartData, prices1, prices2);
+      // setIsLoading(false);
     }
-  }
+    setIsLoading(false);
+  };
   
 
   return (
